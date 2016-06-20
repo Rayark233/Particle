211abdfede07f91bb22b9cd90cdeff1c266627d4
Rayark233 <1512979396@qq.com>
2016-06-21 00:40:46 +0800

    Update Main.php

--- src/Rayark/Main.php
+++ src/Rayark/Main.php
@@ -3,7 +3,7 @@
 namespace Rayark;
 
 use pocketmine\event\Listener;
-use pocketmine
+use pocketmine\plugin\PluginBase:
 use pocketmine
 use pocketmine
 use pocketmine
@@ -15,9 +15,14 @@ use pocketmine
 
 class Main extends PluginBase implements Listener
 {
- public function onLoad()
+ public function Enable()
  {
- 
+  $this->getServe()->getPluginManager()->registerEvent($this,$this);
+  $this->getLogger()->info("Welcome to use ");
  }
-
+ 
+ public function onMove(PlayerMoveEvent $event)
+ {
+  $x=$event()->getPlayer()-$getX();
+ }
 }
