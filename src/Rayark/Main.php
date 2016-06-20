e0a68067b77bc555a678f8947408ad1c81d7aa69
Rayark233 <1512979396@qq.com>
2016-06-21 00:29:46 +0800

    Update Main.php

--- src/Rayark/Main.php
+++ src/Rayark/Main.php
@@ -1,1 +1,23 @@
+<？php
 
+namespace Rayark;
+
+use pocketmine\event\Listener;
+use pocketmine
+use pocketmine
+use pocketmine
+use pocketmine
+use pocketmine
+use pocketmine
+use pocketmine
+use pocketmine
+use pocketmine
+
+class Main extends PluginBase implements Listener
+{
+ public function onLoad()
+ {
+ 
+ }
+
+}
